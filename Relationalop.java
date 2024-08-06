547800d86e479903f12217728571fb7efd2f9e2f
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 19:44:55 +0530

    Add files via upload

---
+++ Relationalop.java
@@ -0,0 +1,39 @@
+public class Relationalop {
+    public static void main(String[] args) {
+        int number= 675;
+        int numbers= 675;
+        System.out.println("the number is:"+(number == numbers));
+
+        int a = 500;
+        int b = 500;
+        int v = 678;
+        int y = 786;
+        System.out.println("the number is :"+(a != b));
+        System.out.println("the number is :"+(y != v));
+
+
+        int i = 500;
+        int g= 500;
+        int k = 678;
+        System.out.println("the number is:"+(k < i));
+        System.out.println("the number is:"+(i > g));
+
+
+        int ii = 980;
+        int gg= 565;
+        int kk = 6878;
+        System.out.println("the number is:"+(gg >= ii));
+        System.out.println("the number is:"+(kk <=gg));
+
+
+
+
+
+
+
+
+    
+
+}
+    
+}
